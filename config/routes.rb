2178b6990ea55c08783c9e66fe3ed8aee65d45e0
Zachary Lehmann <zlehmann@gmail.com>
2019-02-03 14:58:59 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  scope '/admin' do
+    resources :stats, only: [:index]
+  end
 
   root 'posts#index'
 end
